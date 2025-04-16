6060bae0cdafa90c24b92f7026c59e0b847c57e7
SungMinseok <gaegumun@gmail.com>
2025-04-16 13:38:36 +0900

    대규모패치 ui 내부로 이동 등등

--- VideoEncoder.py
+++ VideoEncoder.py
@@ -1,15 +1,12 @@
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5 import uic
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow, QPushButton, QLabel, QLineEdit,
+    QFileDialog, QWidget, QVBoxLayout, QHBoxLayout
+)
 from PyQt5.QtCore import Qt, QThread
-from PyQt5.QtGui import QPixmap
+from datetime import datetime
 import setvideo as sv
 import os
-from PyQt5.QtWidgets import QTreeWidgetItem
-from datetime import datetime
-
-
-form_class = uic.loadUiType('VideoEncoder_UI.ui')[0]
 
 class VideoEncoderThread(QThread):
     def __init__(self, input_filename, output_filename, start_sec, end_sec, bitrate="1500k"):
@@ -19,11 +16,17 @@ class VideoEncoderThread(QThread):
         self.start_sec = start_sec
         self.end_sec = end_sec
         self.bitrate = bitrate
+        print(f"비디오 인코딩 시작: {input_filename} -> {output_filename}")
+        print(f"비트레이트: {bitrate}")
+        print(f"시작 시간: {start_sec}, 종료 시간: {end_sec}")
+
 
     def run(self):
         try:
-            sv.compress_video(self.input_filename, self.output_filename, start_time=self.start_sec, end_time=self.end_sec, bitrate=self.bitrate)
-            #sv.add_text_to_video(self.output_filename, 'rev.33043', self.output_filename)
+            sv.compress_video2(self.input_filename, self.output_filename,
+                              start_time=self.start_sec,
+                              end_time=self.end_sec,
+                              bitrate=self.bitrate)
         except Exception as e:
             print(e)
 
@@ -31,60 +34,189 @@ class VideoEncoderThread(QThread):
         self.terminate()
         self.wait()
 
-class WindowClass(QMainWindow, form_class):
+
+class WindowClass(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setupUi(self)
-        self.setFixedSize(500, 205)
-        self.print_log("실행 가능")
+        #self.setFixedSize(500, 205)
+        self.setWindowTitle("Video Encoder")
         self.setWindowFlags(self.windowFlags() | Qt.WindowStaysOnTopHint)
+
+        self.thread = None
+        self.setup_ui()
+        self.load_config()
+        self.print_log("실행 가능")
+
+    def setup_ui(self):
         
-        config = {}
-        with open('config.txt', 'r', encoding='utf-8') as file:
-            lines = file.readlines()
-        for line in lines:
-            line = line.strip()
-            key, value = line.split('=')
-            config[key] = value
+        self.setStyleSheet("""
+    QWidget {
+        background-color: #1a1a1a;
+        color: #ffffff;
+        /*border-radius: 30px;*/
+        /*border: 1px solid #333333;*/
+        font-family: 'Malgun Gothic', sans-serif;
+        font-size: 11pt;
+        font-weight: bold
+                           
+    }
 
-        self.input_startsec.setText("0")
-        self.input_endsec.setText("0")
-        self.input_bitrate.setText(config['bitrate'])
-        self.input_resultpath.setText(config['save_folder'])
+    QLineEdit {
+        background-color: #333333;
+        border: 1px solid #555555;
+        /*padding: 5px;*/
+        /*border-radius: 5px;*/
+        font-family: 'Malgun Gothic', sans-serif;
+    }
 
-        self.btn_datapath.clicked.connect(lambda: self.select_input_file(self.input_datapath))
+    QTextEdit {
+        background-color: #333333;
+        border: 1px solid #555555;
+        /*padding: 5px;*/
+        /*border-radius: 5px;*/
+        font-family: 'Malgun Gothic', sans-serif;
+    }
+    QPushButton {
+        background-color: #444444;
+        border: 1px solid #666666;
+        /*padding: 5px;*/
+        /*border-radius: 5px;*/
+        font-family: 'Malgun Gothic', sans-serif;
+    }
+
+    QPushButton:hover {
+        background-color: #555555;
+    }
+
+    QPushButton:pressed {
+        background-color: #666666;
+    }
+
+    QComboBox {
+        background-color: #333333;
+        border: 1px solid #555555;
+        /*padding: 5px;*/
+        /*border-radius: 5px;*/
+        font-family: 'Malgun Gothic', sans-serif;
+    }
+
+    QCheckBox {
+        background-color: transparent;
+        border: none;
+        font-family: 'Malgun Gothic', sans-serif;
+    }
+
+    QProgressDialog {
+        background-color: #1a1a1a;
+        color: #ffffff;
+        border-radius: 10px;
+        border: 1px solid #333333;
+        font-family: 'Malgun Gothic', sans-serif;
+    }
+
+    QTimeEdit {
+        background-color: #333333;
+        border: 1px solid #555555;
+        /*padding: 5px;*/
+        /*border-radius: 5px;*/
+        font-family: 'Malgun Gothic', sans-serif;
+    }
+""")
+        
+
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        layout = QVBoxLayout()
+
+        # Input path
+        self.input_datapath = QLineEdit()
+        self.input_datapath.setPlaceholderText("Input file path")
         self.input_datapath.setAcceptDrops(True)
         self.input_datapath.dragEnterEvent = self.drag_enter_event
         self.input_datapath.dropEvent = self.drop_event
 
-        # self.btn_datapath_2.clicked.connect(lambda: self.select_input_file(self.input_datapath_2))
-        # self.input_datapath_2.setAcceptDrops(True)
-        # self.input_datapath_2.dragEnterEvent = self.drag_enter_event
-        # self.input_datapath_2.dropEvent = self.drop_event
+        self.btn_datapath = QPushButton("영상 선택")
+        self.btn_datapath.clicked.connect(lambda: self.select_input_file(self.input_datapath))
 
+        # Start, End, Bitrate
+        self.input_startsec = QLineEdit("0")
+        self.input_endsec = QLineEdit("0")
+        self.input_bitrate = QLineEdit()
+
+        # Result path
+        self.input_resultpath = QLineEdit()
+        self.btn_resultpath = QPushButton("저장 경로")
         self.btn_resultpath.clicked.connect(self.set_directory_path)
-        self.btn_execute.clicked.connect(self.activate)
-        self.btn_execute_2.clicked.connect(self.merge)
 
-        # Add an attribute to keep track of the thread
-        self.thread = None
+        # Result name
+        self.input_resultname = QLineEdit()
+        self.input_resultname.setPlaceholderText("출력 파일명")
 
+        # Buttons
+        self.btn_execute = QPushButton("최신 파일 실행")
+        self.btn_execute_2 = QPushButton("현재 실행")
+        self.btn_execute.clicked.connect(self.activate_latest)
+        self.btn_execute_2.clicked.connect(self.activate)
+
+        # Status
+        self.progressLabel = QLabel()
+
+        # Layout arrangement
+        layout.addWidget(QLabel("입력 영상 경로:"))
+        h1 = QHBoxLayout()
+        h1.addWidget(self.input_datapath)
+        h1.addWidget(self.btn_datapath)
+        layout.addLayout(h1)
+
+        layout.addWidget(QLabel("시작 / 종료 / 비트레이트:"))
+        h2 = QHBoxLayout()
+        h2.addWidget(self.input_startsec)
+        h2.addWidget(self.input_endsec)
+        h2.addWidget(self.input_bitrate)
+        layout.addLayout(h2)
+
+        layout.addWidget(QLabel("저장 경로 및 이름:"))
+        h3 = QHBoxLayout()
+        h3.addWidget(self.input_resultpath)
+        h3.addWidget(self.btn_resultpath)
+        layout.addLayout(h3)
+        layout.addWidget(self.input_resultname)
+
+                # Text Input for overlay
+        self.input_overlay_text = QLineEdit()
+        self.input_overlay_text.setPlaceholderText("영상에 삽입할 텍스트")
+        layout.addWidget(QLabel("오버레이 텍스트:"))
+        layout.addWidget(self.input_overlay_text)
+
+        h4 = QHBoxLayout()
+        h4.addWidget(self.btn_execute)
+        h4.addWidget(self.btn_execute_2)
+        layout.addLayout(h4)
+
+        layout.addWidget(self.progressLabel)
+
+        central_widget.setLayout(layout)
+
+    def load_config(self):
+        config = {}
+        with open('config.txt', 'r', encoding='utf-8') as file:
+            for line in file:
+                key, value = line.strip().split('=')
+                config[key] = value
+        self.input_bitrate.setText(config.get('bitrate', '1500k'))
+        self.input_resultpath.setText(config.get('save_folder', ''))
 
     def select_input_file(self, input_):
-        file_filter = "Video files (*.mp4 *.mkv)"
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        data_file, _ = QFileDialog.getOpenFileName(self, "데이터 파일 선택", directory="C:/Users/mssung/Videos/Captures", filter=file_filter, options=options)
-        try:
-            self.input_datapath.setText(data_file)
+        data_file, _ = QFileDialog.getOpenFileName(self, "데이터 파일 선택",
+                                                   directory="C:/Users/mssung/Videos/Captures",
+                                                   filter="Video files (*.mp4 *.mkv)")
+        if data_file:
+            input_.setText(data_file)
             self.get_video_info()
-        except: 
-            pass
 
     def set_directory_path(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        directory_path = QFileDialog.getExistingDirectory(self, "Select Directory", "", options=options)
+        directory_path = QFileDialog.getExistingDirectory(self, "Select Directory", "")
         if directory_path:
             self.input_resultpath.setText(f'{directory_path}/')
 
@@ -93,16 +225,15 @@ class WindowClass(QMainWindow, form_class):
             event.accept()
         else:
             event.ignore()
-    
+
     def drop_event(self, event):
-        urls = event.mimeData().urls()
-        file_path = urls[0].toLocalFile()
+        file_path = event.mimeData().urls()[0].toLocalFile()
         self.input_datapath.setText(file_path)
         self.get_video_info()
 
     def get_video_info(self):
         video_path = self.input_datapath.text()
-        if video_path == "":
+        if not video_path:
             return
         video_info = sv.get_videoinfo(video_path)
         self.input_startsec.setText('0')
@@ -112,78 +243,51 @@ class WindowClass(QMainWindow, form_class):
         self.progressLabel.setText(log)
         QApplication.processEvents()
 
-    def activate(self):
-
-#
-        self.input_datapath.setText(self.set_most_recent_file(fr'C:/Users/mssung/Videos/Captures/'))
+    def activate_latest(self):
+        self.input_datapath.setText(self.set_most_recent_file('C:/Users/mssung/Videos/Captures/'))
         self.get_video_info()
+        self.encode_video()
 
+    def activate(self):
+        self.encode_video()
 
+    def encode_video(self):
         input_filename = self.input_datapath.text()
         output_path = self.input_resultpath.text()
         start_sec = float(self.input_startsec.text())
         end_sec = float(self.input_endsec.text())
-        tempname = self.input_resultname.text()
+        tempname = self.input_resultname.text().strip()
+        if not tempname:
+            tempname = datetime.now().strftime('%y%m%d_%H%M')
+            self.input_resultname.setText(tempname)
 
+        output_filename = f'{output_path}{tempname}.mp4'
 
-        
-        if tempname == "":
-            tempname = "result"
-
-        current_time = datetime.now().strftime('%y%m%d_%H%M')
-        self.input_resultname.setText(current_time)
-
-        # Create the output filename in 'yymmdd_hhmm.mp4' format
-        output_filename = f'{output_path}{current_time}.mp4'
-        #output_filename = f'{output_path}{tempname}.mp4'
-
-        # Stop the previous thread if it is still running
-        if self.thread is not None and self.thread.isRunning():
+        if self.thread and self.thread.isRunning():
             self.thread.stop()
 
-        self.thread = VideoEncoderThread(input_filename, output_filename, start_sec, end_sec)
+        self.thread = VideoEncoderThread(input_filename, output_filename, start_sec, end_sec, self.input_bitrate.text())
         self.thread.finished.connect(self.on_thread_finished)
         self.thread.start()
 
-
-    def set_most_recent_file(self,folder_path):
+    def set_most_recent_file(self, folder_path):
         try:
-            # Get all files in the directory
             files = [f for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-            
-            # Check if folder is empty
             if not files:
-                print("No files found in the specified folder.")
+                print("No files found.")
                 return None
-            
-            # Get the most recent file based on modification time
             most_recent_file = max(files, key=lambda f: os.path.getmtime(os.path.join(folder_path, f)))
-            input_filename = os.path.join(folder_path, most_recent_file)
-            print(f"Most recent file: {input_filename}")
-            return input_filename
+            return os.path.join(folder_path, most_recent_file)
         except Exception as e:
-            print(f"Error while fetching the most recent file: {e}")
+            print("Error:", e)
             return None
-        
-    def merge(self):
-        video_0 = self.input_datapath.text()
-        video_1 = self.input_datapath_2.text()
-        result_dir = self.input_resultpath.text()
-        tempname = self.input_resultname.text()
-        if tempname == "":
-            tempname = "result"
-        output_filename = f'{result_dir}{tempname}.mp4'
-        #merge_videos(video_0, video_1, output_filename)
 
     def on_thread_finished(self):
         print("Thread finished")
-        # Additional cleanup if needed
-        output_path = self.input_resultpath.text()
-        os.startfile(output_path)
-
+        os.startfile(self.input_resultpath.text())
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    myWindow = WindowClass()
-    myWindow.show()
-    app.exec_()
+    window = WindowClass()
+    window.show()
+    sys.exit(app.exec_())
